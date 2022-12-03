135a138075a39bd122695a24d400f6408644a485
GeraGarro <113718948+GeraGarro@users.noreply.github.com>
2022-12-03 15:29:26 -0300

    Armado final de la pagina principal html css java

--- json/app.js
+++ json/app.js
@@ -1,8 +1,18 @@
 const menu= document.querySelector('.boton-hamburgueza');
 const navegacion= document.querySelector('.navegacion')
 const imagenes = document.querySelectorAll('img');
+const btnTodos=document.querySelector('.Todos');
+
+const btnDescartables=document.querySelector('.descartables');
+const btnProtesis=document.querySelector('.protesis');
+const btnCirugia=document.querySelector('.cirugia');
+const btnEndodoncia=document.querySelector('.endodoncia');
+const btnAnestesia=document.querySelector('.anestesia');
+const contenedorProductos =document.querySelector ('.productos');
+
 document.addEventListener('DOMContentLoaded',()=>{
    eventos();
+   productos();
 });
 
 const eventos = () =>{
@@ -36,6 +46,8 @@ const botonCerrar = () =>{
 }
 
 
+
+
 const cerrarMenu = (boton,overlay) =>{
 boton.addEventListener('click',()=>{
     navegacion.classList.add('ocultar');
@@ -49,7 +61,92 @@ overlay.onclick = function(){
     boton.remove();
 }
 }
+// A medida que se deja de observar la imagen se desaparezcan de la page
+
+const observer = new IntersectionObserver((entries,observer)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            const imagen =entry.target;
+            imagen.src = imagen.dataset.src;
+            observer.unobserve(imagen);
+        }
+    })
+});
+
+// A medida que recorres la pagina se vayan agregando las imagenes a la page
 
 imagenes.forEach(imagen=>{
-    imagen.src = imagen.dataset.src;
+    
+    observer.observe(imagen);
 })
+
+// funcion para filtrar las categorias
+
+const productos = () => {
+    let productosArreglo =[];
+    const productos = document.querySelectorAll('.producto');
+    productos.forEach(producto=>productosArreglo=[...productosArreglo,producto])
+    
+    const arrDescartables = productosArreglo.filter(descartables=> descartables.getAttribute('data-producto')==='descartable');
+
+    
+    const arrProtesis = productosArreglo.filter(protesis=> protesis.getAttribute('data-producto')==='protesis');
+
+    const arrCirugias = productosArreglo.filter(cirugia=> cirugia.getAttribute('data-producto')==='cirugia');
+
+    const arrEndodoncias = productosArreglo.filter(endodoncia=> endodoncia.getAttribute('data-producto')==='endodoncia');
+
+    const arrAnestesias = productosArreglo.filter(anestesia=> anestesia.getAttribute('data-producto')==='anestesia');
+   
+    mostrarProductos(arrDescartables,arrProtesis,arrCirugias,arrEndodoncias,arrAnestesias,productosArreglo);
+
+}
+
+const mostrarProductos = (arrDescartables,arrProtesis,arrCirugias,arrEndodoncias,arrAnestesias, todos)=> {
+    btnDescartables.addEventListener('click',()=>{
+        limpiarPagina(contenedorProductos);
+        arrDescartables.forEach(descartables => contenedorProductos.appendChild(descartables));
+    });
+    
+    btnCirugia.addEventListener('click',()=>{
+        limpiarPagina(contenedorProductos);
+        arrCirugias.forEach(cirugia => contenedorProductos.appendChild(cirugia));
+    });
+
+    
+
+   btnEndodoncia.addEventListener('click',()=>{
+        limpiarPagina(contenedorProductos);
+        arrEndodoncias.forEach(endodoncia => contenedorProductos.appendChild(endodoncia));
+    });
+
+    btnProtesis.addEventListener('click',()=>{
+        limpiarPagina(contenedorProductos);
+        arrProtesis.forEach(protesis => contenedorProductos.appendChild(protesis));
+    });
+
+    btnAnestesia.addEventListener('click',()=>{
+        limpiarPagina(contenedorProductos);
+        arrAnestesias.forEach(anestesia => contenedorProductos.appendChild(anestesia));
+    });
+
+   btnTodos.addEventListener('click',()=>{
+    limpiarPagina(contenedorProductos);
+    todos.forEach(todo=>contenedorProductos.appendChild(todo));
+   })
+
+    
+
+
+
+}
+
+
+
+const limpiarPagina = (contenedor) =>{
+    while(contenedor.firstChild){
+        contenedor.removeChild(contenedor.firstChild);
+    
+    }
+}
+
